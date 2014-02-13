000b605ce6c0e41b74b940eee787dc055187a91a
Ethan Lincoln <ethan@glympse.com>
2014-02-12 16:11:03 -0800

    iOS: Updated UriParser's urlEncode helper method to cover cases Apple's default impl. does not + Minor UI tweaks to demo

--- iOS/GlympseSchemeLib/GlympseSchemeLib/GLYUriParser.m
+++ iOS/GlympseSchemeLib/GlympseSchemeLib/GLYUriParser.m
@@ -221,9 +221,60 @@ static const int GLYMaximumCodeLength = 128;
     return dict;
 }
 
+
+static const char URLSAFE[256] =
+{
+    /*      0 1 2 3  4 5 6 7  8 9 A B  C D E F */
+    /* 0 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 1 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 2 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,1,0,0,
+    /* 3 */ 1,1,1,1, 1,1,1,1, 1,1,0,0, 0,0,0,0,
+    
+    /* 4 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+    /* 5 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,1,
+    /* 6 */ 0,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1,
+    /* 7 */ 1,1,1,1, 1,1,1,1, 1,1,1,0, 0,0,0,0,
+    
+    /* 8 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* 9 */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* A */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* B */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    
+    /* C */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* D */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* E */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
+    /* F */ 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0
+};
+
 + (NSString*)urlEncode:(NSString*)str
 {
-    return [str stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    //NSString and CFString % encoding helpers do not handle & or / correctly, so we do it ourselves:
+    const char DEC2HEX[16 + 1] = "0123456789ABCDEF";
+    const unsigned char * pSrc = (const unsigned char *)[str UTF8String];
+    const NSUInteger SRC_LEN = str.length;
+    unsigned char * const pStart = malloc((SRC_LEN * 3 + 1) * sizeof(unsigned char));
+    unsigned char * pEnd = pStart;
+    const unsigned char * const SRC_END = pSrc + SRC_LEN;
+    for (; pSrc < SRC_END; ++pSrc)
+	{
+		if ( URLSAFE[*pSrc] )
+        {
+            *pEnd++ = *pSrc;
+        }
+        else
+        {
+            // escape this char
+            *pEnd++ = '%';
+            *pEnd++ = DEC2HEX[*pSrc >> 4];
+            *pEnd++ = DEC2HEX[*pSrc & 0x0F];
+        }
+	}
+    // Put string terminator.
+    *pEnd = '\0';
+    // Copy the temporary c-str to a new, autoreleasing NSString
+    NSString *output = [NSString stringWithUTF8String:(char*)pStart];
+    free(pStart);
+    return output;
 }
 
 + (NSString*)urlDecode:(NSString*)str
