16c9539dd0a26f46b836520eb1a0270e787a2bca
Steve Miller <steve@glympse.com>
2014-02-18 15:19:49 -0800

    Added isCreateOnly() to Recipient class.

--- Android/GlympseIntentsLib/src/com/glympse/android/intent/Recipient.java
+++ Android/GlympseIntentsLib/src/com/glympse/android/intent/Recipient.java
@@ -152,6 +152,11 @@ public class Recipient
         return _address;
     }
 
+    public boolean isCreateOnly()
+    {
+        return _createOnly;
+    }
+
     public String getUrl()
     {
         return _url;
